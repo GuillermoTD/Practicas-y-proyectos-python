91aaefb8857a47c5018ca522dc5b7dbdeae3d99d
GuillermoTD <guillermotd23@gmail.com>
2022-11-30 17:37:09 -0400

    lo que se hizo 30 noviembre 2022 version 2

--- SPOJ_Julius_Caesar_Cipher_13115/Julius_Caesar_Cipher.py
+++ SPOJ_Julius_Caesar_Cipher_13115/Julius_Caesar_Cipher.py
@@ -1,12 +1,40 @@
 
-def cipher(text="",s=2):
-    Abcdario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    nuevoTexto=""
+def cipher(text="KLK"):
+    Abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    nuevoTexto = ""
 
     for letra in text:
-        indice = Abcdario.index(letra)
-        nuevoIndice = (indice+2)%26
-        nuevoTexto = nuevoTexto + Abcdario[nuevoIndice]
+        indice = Abecedario.index(letra)
+        nuevoIndice = (indice+2) % 26
+        nuevoTexto += Abecedario[nuevoIndice]
     print(nuevoTexto)
 
-cipher()
+
+def deciphered(text="MNM"):
+    Abcdario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    nuevoTexto = ""
+    for letra in text:
+        indice = Abcdario.index(letra)
+        nuevoIndice = (indice-2) % 26
+        nuevoTexto += Abcdario[nuevoIndice]
+    print(nuevoTexto)
+
+
+# cipher()
+# deciphered()
+
+def get_count(string):
+    #Return the number (count) of vowels in the given string.
+    vowels = ['a','e','i','o','u']
+    vowelsCount = 0
+    
+    for n in string:
+        index = vowels.find(n)
+        if n == vowels[index]:
+            vowelsCount +=1
+    print(vowelsCount)
+
+
+get_count("miralo")
